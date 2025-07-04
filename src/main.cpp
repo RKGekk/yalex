419c4a6508c8640d34e64297056b165beb1910fd
o.arkhangelsky <o.arkhangelsky@vk.team>
2025-07-04 17:14:31 +0300

    ast and parse added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 #include "lexer.h"
+#include "parse.h"
 #include "runtime.h"
+#include "statement.h"
 #include "test_runner_p.h"
 
 namespace parse {
@@ -13,24 +15,119 @@ namespace runtime {
     void RunObjectsTests(TestRunner& tr);
 }
 
+namespace ast {
+    void RunUnitTests(TestRunner& tr);
+}
+
+void TestParseProgram(TestRunner& tr);
+
 namespace {
+    void RunMythonProgram(std::istream& input, std::ostream& output) {
+        parse::Lexer lexer(input);
+        auto program = ParseProgram(lexer);
+
+        runtime::SimpleContext context{output};
+        runtime::Closure closure;
+        program->Execute(closure, context);
+    }
+
+    void TestSimplePrints() {
+    std::istringstream input(R"(
+print 57
+print 10, 24, -8
+print 'hello'
+print "world"
+print True, False
+print
+print None
+)");
+
+    std::ostringstream output;
+    RunMythonProgram(input, output);
+
+    ASSERT_EQUAL(output.str(), "57\n10 24 -8\nhello\nworld\nTrue False\n\nNone\n");
+}
+
+void TestAssignments() {
+    std::istringstream input(R"(
+x = 57
+print x
+x = 'C++ black belt'
+print x
+y = False
+x = y
+print x
+x = None
+print x, y
+)");
+
+    std::ostringstream output;
+    RunMythonProgram(input, output);
+
+    ASSERT_EQUAL(output.str(), "57\nC++ black belt\nFalse\nNone False\n");
+}
+
+void TestArithmetics() {
+    std::istringstream input("print 1+2+3+4+5, 1*2*3*4*5, 1-2-3-4-5, 36/4/3, 2*5+10/2");
+
+    std::ostringstream output;
+    RunMythonProgram(input, output);
+
+    ASSERT_EQUAL(output.str(), "15 120 -13 3 15\n");
+}
+
+void TestVariablesArePointers() {
+    std::istringstream input(R"(
+class Counter:
+  def __init__():
+    self.value = 0
+
+  def add():
+    self.value = self.value + 1
+
+class Dummy:
+  def do_add(counter):
+    counter.add()
+
+x = Counter()
+y = x
+
+x.add()
+y.add()
+
+print x.value
+
+d = Dummy()
+d.do_add(x)
+
+print y.value
+)");
+
+    std::ostringstream output;
+    RunMythonProgram(input, output);
+
+    ASSERT_EQUAL(output.str(), "2\n3\n");
+}
+
     void TestAll() {
         TestRunner tr;
         parse::RunOpenLexerTests(tr);
         runtime::RunObjectHolderTests(tr);
         runtime::RunObjectsTests(tr);
+        ast::RunUnitTests(tr);
+        TestParseProgram(tr);
+
+        RUN_TEST(tr, TestSimplePrints);
+        RUN_TEST(tr, TestAssignments);
+        RUN_TEST(tr, TestArithmetics);
+        RUN_TEST(tr, TestVariablesArePointers);
     }
 }  // namespace
 
 int main() {
     try {
         TestAll();
-        parse::Lexer lexer(std::cin);
-        parse::Token t;
-        while ((t = lexer.CurrentToken()) != parse::token_type::Eof{}) {
-            std::cout << t << std::endl;
-            lexer.NextToken();
-        }
+        RunMythonProgram(std::cin, std::cout);
     }
     catch (const std::exception& e) {
         std::cerr << e.what();
